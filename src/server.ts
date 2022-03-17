db7f349e9ddd78692b5558029ef3a4f8215051b6
Pablo Pessanha <pablopessanh@gmail.com>
2022-03-17 13:40:56 -0300

    lint: adds semicolon

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   return res.status(200).json({ message: 'ok' });
